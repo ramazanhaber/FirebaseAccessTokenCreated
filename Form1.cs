5164f76c973fc3f34ffed0b78dbc61ba01d1373d
RAMBO <RAMBO@RAMBO3>
2024-05-23 13:02:45 +0300

    yeni güncelleme 23.05.2024

--- Form1.cs
+++ Form1.cs
@@ -8,6 +8,15 @@ namespace FirebaseAccessTokenCreated
 {
     public partial class Form1 : Form
     {
+
+        /*
+         1- Firebase console
+         2- Project setttings
+         3- Service Accounds
+         4- Generate new private key
+         5- rename json file adminsdk.json
+         6- debug move file -> adminsdk.json
+        */
         public Form1()
         {
             InitializeComponent();
