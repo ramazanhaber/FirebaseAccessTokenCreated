055fe4691aa49c4bfdd97ea10d02e5ffdb34c8bd
RAMBO <RAMBO@RAMBO3>
2024-05-23 13:01:11 +0300

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace FirebaseAccessTokenCreated
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtAccessToken = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(383, 70);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Access Token Oluştur";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtAccessToken
+            // 
+            this.txtAccessToken.Location = new System.Drawing.Point(12, 88);
+            this.txtAccessToken.Multiline = true;
+            this.txtAccessToken.Name = "txtAccessToken";
+            this.txtAccessToken.Size = new System.Drawing.Size(383, 317);
+            this.txtAccessToken.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtAccessToken);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Firebase V1 Push Notification Cloud Message ";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtAccessToken;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,56 @@
+﻿using FirebaseAdmin;
+using Google.Apis.Auth.OAuth2;
+using System;
+using System.Windows.Forms;
+
+
+namespace FirebaseAccessTokenCreated
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            InitializeFirebase();
+        }
+        private void InitializeFirebase()
+        {
+            try
+            {
+                string jsonPath = "adminsdk.json"; // JSON dosyanızın yolu
+                Environment.SetEnvironmentVariable("GOOGLE_APPLICATION_CREDENTIALS", jsonPath);
+
+                var credential = GoogleCredential.FromFile(jsonPath).CreateScoped(new[]
+                {
+                "https://www.googleapis.com/auth/firebase.messaging"
+            });
+
+                if (FirebaseApp.DefaultInstance == null)
+                {
+                    FirebaseApp.Create(new AppOptions()
+                    {
+                        Credential = credential,
+                    });
+                }
+
+                var token = credential.UnderlyingCredential.GetAccessTokenForRequestAsync().Result;
+
+                Clipboard.SetText(token);
+
+                txtAccessToken.Text = token;
+                MessageBox.Show("Panoya Kopyalandi -> Access Token: " + token);
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error initializing Firebase: " + ex.Message);
+            }
+        }
+
+
+    }
+}
